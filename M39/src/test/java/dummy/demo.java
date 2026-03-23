8bbfed57690bd43d433225e19a7f9d84c45d7c3b
abhijeet kalagate <abhijeet kalagate@LAPTOP-VFKL1BGO>
2026-03-23 09:56:35 +0530

    commit

---
+++ M39/src/test/java/dummy/demo.java
@@ -0,0 +1,11 @@
+package dummy;
+
+public class demo {
+
+	public static void main(String[] args) {
+		System.out.println("hello everyone");
+		System.out.println("hello everyone");
+		System.out.println("hello everyone");
+	}
+
+}
